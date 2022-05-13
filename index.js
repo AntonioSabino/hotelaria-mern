c7839727b754c03e20f2a8f04d46902f7351ce30
Antonio Sabino <aslimajr@outlook.com>
2022-05-13 11:23:30 -0300

    Conexão mongoDB

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB desconectado!");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB conectado!");
+});
+
 app.listen(process.env.PORT, () => {
+  connect();
   console.log(`Rodando em ${process.env.BASE}`);
 });
