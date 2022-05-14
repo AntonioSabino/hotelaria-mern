507adc0004dd0e5d468802bf631e3bc19d92accc
Antonio Sabino <aslimajr@outlook.com>
2022-05-14 12:20:39 -0300

    Implementando o bcrypt ao password

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -1,11 +1,15 @@
 import Users from "../models/Users.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res, next) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new Users({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
 
     await newUser.save();
