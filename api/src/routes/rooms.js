551d8f6ffe0278558a7b8c921def4a40380995ad
Antonio Sabino <aslimajr@outlook.com>
2022-05-15 17:51:38 -0300

    Criando rotas para rooms

--- api/src/routes/rooms.js
+++ api/src/routes/rooms.js
@@ -1,5 +1,28 @@
 import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+} from "../controllers/roomController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+//Create a hotel
+router.post("/:hotelId", verifyAdmin, createRoom);
+
+//Updata a hotel
+router.put("/:id", verifyAdmin, updateRoom);
+
+//Delete a hotel
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom);
+
+//Get a hotel by id
+router.get("/:id", getRoom);
+
+//Get all hotels
+router.get("/", getRooms);
 
 export default router;
