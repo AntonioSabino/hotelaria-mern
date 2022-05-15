642ee47f1a3b9ca7d751bd652b701f9ed1d0e9b1
Antonio Sabino <aslimajr@outlook.com>
2022-05-15 13:43:11 -0300

    Criando rotas users/checkauth e checkuser/:id

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -5,8 +5,17 @@ import {
   getUsers,
   updateUser,
 } from "../controllers/userController.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauth", verifyToken, (req, res, next) => {
+  res.send("Olá, você está logado.");
+});
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+  res.send("Olá, você está logado e pode apagar a sua conta.");
+});
 
 //Updata a Users
 router.put("/:id", updateUser);
