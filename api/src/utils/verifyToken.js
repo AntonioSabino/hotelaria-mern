562a5e006dcea82715439d5af8b60a2c5176d610
Antonio Sabino <aslimajr@outlook.com>
2022-05-15 13:42:30 -0300

    Criando verifyUser

--- api/src/utils/verifyToken.js
+++ api/src/utils/verifyToken.js
@@ -1,16 +1,26 @@
 import jwt from "jsonwebtoken";
-import { createError } from "./error";
+import { createError } from "./error.js";
 
-export const verifyToken = (req, _res, next) => {
+export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
     return next(createError(401, "Autenticação inválida."));
   }
 
-  jwt.verify(token, process.envJWT, (error, user) => {
+  jwt.verify(token, process.env.JWT, (error, user) => {
     if (error) return next(createError(403, "Token inválido."));
 
     req.user = user;
     next();
+  });
+};
+
+export const verifyUser = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, "Você não está autorizado."));
+    }
   });
 };
